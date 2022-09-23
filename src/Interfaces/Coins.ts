b2d1972598c6156761007418f723f2dace8c9cd9
RomelLopez <romelanthonylopez@gmail.com>
2022-09-22 19:34:57 -0700

    installed gh pages, built and deployed

---
+++ src/Interfaces/Coins.ts
@@ -0,0 +1,10 @@
+export default interface Coins {
+    name: string,
+    image: string,
+    symbol: string,
+    price: number,
+    volume: number,
+    priceChange: number,
+    marketcap: number
+}
+
