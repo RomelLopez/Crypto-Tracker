3611c539f094f51d5cfa11f3373f6048a9e966f8
RomelLopez <romelanthonylopez@gmail.com>
2022-09-24 01:40:04 -0700

    added title to data and began web responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Coin from './Coin';
+import './Coin.css';
 import Cryptos from './Interfaces/Cryptos';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
 
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false')
       .then(res => {
         setCoins(res.data)
       }).catch(error => console.log(error))
@@ -33,6 +34,20 @@ function App() {
           <input type="text" placeholder='Search'
             className="coin-input" onChange={handleChange} />
         </form>
+      </div>
+      <div className='coin-row-title'>
+        <div className='coin-title'>
+          <h1>Name</h1>
+          <p>Symbol</p>
+        </div>
+        <div className="coin-data-title">
+          <p className="coin-price-title">Price</p>
+          <p className="coin-volume-title">Volume</p>
+          <p className="coin-percent-title">Change</p>
+          <p className="coin-marketcap-title">
+            Market Cap
+          </p>
+        </div>
       </div>
       {filteredCoins.map(coin => {
         return (
